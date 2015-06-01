e71a3c1dc4e78f05059b0d433d94e66682d6dbd7
Jon B <jondbridges@gmail.com>
2015-06-01 18:03:52 -0500

    AddListener method added

--- Assets/Scripts/NotificationsManager.cs
+++ Assets/Scripts/NotificationsManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class NotificationsManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Dictionary<string, List<Component>> Listeners = new Dictionary<string, List<Component>>();
+
+    public void AddListener(Component listener, string notificationName) {
+        
+        // Add listener to dictionary
+        if (!Listeners.ContainsKey(notificationName)) {
+            Listeners.Add(notificationName, new List<Component>());
+        }
+
+        // Add object to listener list for this notification
+        Listeners[notificationName].Add(listener);
+    }
 }
