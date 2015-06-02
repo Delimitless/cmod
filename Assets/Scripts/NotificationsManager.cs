19931fa95975e65fa6847015b1db4273ef855ba6
Jon B <jondbridges@gmail.com>
2015-06-01 20:02:27 -0500

    Add up to RemoveRedundancies (pg 103)

--- Assets/Scripts/NotificationsManager.cs
+++ Assets/Scripts/NotificationsManager.cs
@@ -3,16 +3,59 @@ using System.Collections.Generic;
 
 public class NotificationsManager : MonoBehaviour {
 
-    private Dictionary<string, List<Component>> Listeners = new Dictionary<string, List<Component>>();
+    private Dictionary<string, List<Component>> listeners = new Dictionary<string, List<Component>>();
 
     public void AddListener(Component listener, string notificationName) {
         
         // Add listener to dictionary
-        if (!Listeners.ContainsKey(notificationName)) {
-            Listeners.Add(notificationName, new List<Component>());
+        if (!listeners.ContainsKey(notificationName)) {
+            listeners.Add(notificationName, new List<Component>());
         }
 
         // Add object to listener list for this notification
-        Listeners[notificationName].Add(listener);
+        listeners[notificationName].Add(listener);
+    }
+
+    public void RemoveListener(Component listener, string notificationName) {
+        
+        if (!listeners.ContainsKey(notificationName)) {
+            return;
+        }
+
+        for (int i = listeners[notificationName].Count - 1; i >= 0; i--) {
+            if (listeners[notificationName][i].GetInstanceID() == listener.GetInstanceID()) {
+                listeners[notificationName].RemoveAt(i);
+            }
+        }
+    }
+
+    public void PostNotification(string notificationName) {
+
+        if (!listeners.ContainsKey(notificationName)) {
+            return;
+        }
+
+        foreach (Component listener in listeners[notificationName]) {
+            listener.SendMessage(notificationName, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public void RemoveRedundancies() {
+        
+        Dictionary<string, List<Component>> tmpListeners = new Dictionary<string, List<Component>>();
+
+        foreach (KeyValuePair<string, List<Component>> item in listeners) {
+            for (int i = item.Value.Count - 1; i >= 0; i--) {
+                if (item.Value[i] == null) {
+                    item.Value.RemoveAt(i);
+                }
+            }
+
+            if (item.Value.Count > 0) {
+                tmpListeners.Add(item.Key, item.Value);
+            }
+        }
+
+        listeners = tmpListeners;
     }
 }
