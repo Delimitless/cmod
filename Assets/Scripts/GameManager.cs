ecdc993339b99224116bc6d2a6ac8bfda9c82fe9
Jon B <jondbridges@gmail.com>
2015-06-02 22:14:32 -0500

    Added game manger with singleton (pg 137)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NotificationsManager))]
+public class GameManager : Singleton<GameManager> {
+
+    protected GameManager() { }
+
+    private static NotificationsManager notifications = null;
+
+    public static NotificationsManager Notifications {
+        get {
+            if (notifications == null) {
+                notifications = GameManager.Instance.GetComponent<NotificationsManager>();
+            }
+
+            return notifications;
+        }
+    }
+}
--- Assets/Scripts/PowerupDollar.cs
+++ Assets/Scripts/PowerupDollar.cs
@@ -7,4 +7,32 @@ public class PowerupDollar : MonoBehaviour {
     public AudioClip clip = null;
 
     private AudioSource sfx = null;
+
+    void Start() {
+        GameObject soundsObject = GameObject.FindGameObjectWithTag("sounds");
+
+        if (soundsObject == null) {
+            return;
+        }
+
+        sfx = soundsObject.GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("player")) {
+            return;
+        }
+
+        if (sfx) {
+            sfx.PlayOneShot(clip, 1.0f);
+        }
+
+        gameObject.SetActive(false);
+
+//        PlayerController pc = other.gameObject.GetComponent<PlayerController>();
+//
+//        if (pc) {
+//            pc.Cash += cashAmount;
+//        }
+    }
 }
